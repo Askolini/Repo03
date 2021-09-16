00bf604cd15ec859935abee2e53a0b7e215b337a
Askolini <asmircc@gmail.com>
2021-09-16 04:57:14 +0200

    Sum

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //
+    var numOne;
+    var numTwo;
+    var sum = numOne + numTwo;
+    //
+
+    return sum;
+}
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
+
+module.exports = main;
