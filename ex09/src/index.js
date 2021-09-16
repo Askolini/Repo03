0761c809e50005673580c8ab2ddd809131a8b0e1
Askolini <asmircc@gmail.com>
2021-09-16 05:01:38 +0200

    Difference

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //
+    var numOne;
+    var numTwo;
+    var difference = numOne - numTwo;
+    //
+
+    return difference;
+}
+console.log(main(15, 8));
+console.log(main(25, 8));
+console.log(main(425, 120));
+
+module.exports = main;
