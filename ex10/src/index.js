bc7c21f1715fdd33133adcbde49e410a552b7a73
Askolini <asmircc@gmail.com>
2021-09-16 05:04:59 +0200

    Multiplying

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //
+    var numOne;
+    var numTwo;
+    var product = numOne * numTwo;
+    //
+
+    return product;
+}
+console.log(main(15, 3));
+console.log(main(25, 8));
+console.log(main(425, 10));
+
+module.exports = main;
